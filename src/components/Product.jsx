5fa0382c412a8edb2765ce2c2d5dfe07f083dae9
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2025-02-03 19:54:14 +0100

    add Product

---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+export default function Product() {
+  const price = 900;
+  const imgUrl =
+    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640';
+
+  return (
+    <div>
+      <h2>Tacos</h2>
+      <img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <p>Price: {price + 50} credits</p>
+    </div>
+  );
+}
